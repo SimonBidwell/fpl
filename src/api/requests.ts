3962287aa6dad62bf781ee2d400c2ac8086002fd
Simon Bidwell <simon@ludonautics.com>
2025-12-05 01:09:23 +0000

    updating vite

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,48 +1,48 @@
 import {
-    bootstrapStaticSchema,
-    leagueDetailsSchema,
-    elementStatusResponseSchema,
-    choicesResponseSchema,
-    transactionsResponseSchema,
+  bootstrapStaticSchema,
+  choicesResponseSchema,
+  elementStatusResponseSchema,
+  leagueDetailsSchema,
+  transactionsResponseSchema,
 } from "./domain";
 
 export const getLeagueDetails = async (seasonId: string) => {
-    const res = await fetch(`${import.meta.env.BASE_URL}/${seasonId}.json`);
-    return leagueDetailsSchema.parse(await res.json());
+  const res = await fetch(`${import.meta.env.BASE_URL}/${seasonId}.json`);
+  return leagueDetailsSchema.parse(await res.json());
 };
 
 export const getBootstrapStatic = async (seasonId: string) => {
-    const res = await fetch(
-        `${import.meta.env.BASE_URL}/bootstrap-static-${seasonId}.json`
-    );
-    return bootstrapStaticSchema.parse(await res.json());
+  const res = await fetch(
+    `${import.meta.env.BASE_URL}/bootstrap-static-${seasonId}.json`
+  );
+  return bootstrapStaticSchema.parse(await res.json());
 };
 
 export const getElementStatus = async (seasonId: string) => {
-    const res = await fetch(
-        `${import.meta.env.BASE_URL}/element-status-${seasonId}.json`
-    );
-    return elementStatusResponseSchema.parse(await res.json());
+  const res = await fetch(
+    `${import.meta.env.BASE_URL}/element-status-${seasonId}.json`
+  );
+  return elementStatusResponseSchema.parse(await res.json());
 };
 
 export const getDraftChoices = async (seasonId: string) => {
-    const res = await fetch(
-        `${import.meta.env.BASE_URL}/draft-choices-${seasonId}.json`
-    );
-    if (res.ok) {
-        return choicesResponseSchema.parse(await res.json());
-    } else {
-        return undefined;
-    }
+  const res = await fetch(
+    `${import.meta.env.BASE_URL}/draft-choices-${seasonId}.json`
+  );
+  if (res.ok && !res.headers.get("content-type")?.includes("text/html")) {
+    return choicesResponseSchema.parse(await res.json());
+  } else {
+    return undefined;
+  }
 };
 
 export const getTransactions = async (seasonId: string) => {
-    const res = await fetch(
-        `${import.meta.env.BASE_URL}/transactions-${seasonId}.json`
-    );
-    if (res.ok) {
-        return transactionsResponseSchema.parse(await res.json());
-    } else {
-        return undefined;
-    }
+  const res = await fetch(
+    `${import.meta.env.BASE_URL}/transactions-${seasonId}.json`
+  );
+  if (res.ok && !res.headers.get("content-type")?.includes("text/html")) {
+    return transactionsResponseSchema.parse(await res.json());
+  } else {
+    return undefined;
+  }
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/fpl",
-    plugins: [react()],
-    build: {
-        rollupOptions: {
-          input: {
-            index: 'index.html',
-            //include 404 file for gh-pages SPA workaround
-            "404": '404.html',
-          },
-        },
-    }
+  base: "/fpl",
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: "index.html",
+        //include 404 file for gh-pages SPA workaround
+        "404": "404.html",
+      },
+    },
+  },
 });
